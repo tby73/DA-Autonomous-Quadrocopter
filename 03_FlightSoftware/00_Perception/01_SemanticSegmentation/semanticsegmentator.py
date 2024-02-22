f46f5384f724a386e0f1f5b060b3ff2c1fc65d0c
Oliver Pauer <83162717+tby73@users.noreply.github.com>
2024-02-22 11:04:31 +0100

    Seg complete

--- 03_FlightSoftware/00_Perception/01_SemanticSegmentation/semanticsegmentator.py
+++ 03_FlightSoftware/00_Perception/01_SemanticSegmentation/semanticsegmentator.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 class SemanticSegmentator:
-    def __init__(self, image, model_path, output_class_file) -> None:
+    def __init__(self, image, model_path, output_class_file, set_default_colors=True, color_file_path="colors.txt") -> None:
         # load input image and model
         self.image = cv2.resize(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), (640, 480), interpolation=cv2.INTER_CUBIC)
         self.model = cv2.dnn.readNet(model_path)
@@ -11,9 +11,14 @@ class SemanticSegmentator:
         self.output_classes = open(output_class_file).read().strip().split("\n")
 
         # generate colors
-        np.random.seed(42)
-        self.colors = np.random.randint(0, 255, size=(len(self.output_classes - 1)), dtype=np.uint8)
-        self.colors = np.vstack([[0, 0, 0], self.colors]).astype(np.uint8)
+        if set_default_colors:
+            self.colors = open(color_file_path).read().strip().split("\n")
+            self.colors = [np.array(c.split(",")).astype("int") for c in self.colors]
+            self.colors = np.array(self.colors, dtype=np.uint8)
+        else:
+            np.random.seed(42)
+            self.colors = np.random.randint(0, 255, size=(len(self.output_classes) - 1, 3), dtype=np.uint8)
+            self.colors = np.vstack([[0, 0, 0], self.colors]).astype(np.uint8)
 
     def Segment(self):
         # create blob
@@ -30,7 +35,7 @@ class SemanticSegmentator:
         # construct mask and overlay
         mask = self.colors[seg_map]
         mask = cv2.resize(mask, (self.image.shape[1], self.image.shape[0]), interpolation=cv2.INTER_NEAREST)
-        output = cv2.addWeighted(output, 0.3, mask, 0.7, 0)
+        output = cv2.addWeighted(self.image, 0.3, mask, 0.7, 0)
 
         return output
 
