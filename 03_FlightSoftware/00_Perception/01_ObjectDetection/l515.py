afa1fec36a29d555f9bfee1dc21c5a0284752126
Oliver Pauer <83162717+tby73@users.noreply.github.com>
2024-02-25 16:07:43 +0100

    Added RS2 to OBJ-D and SemSeg interference

--- 03_FlightSoftware/00_Perception/01_ObjectDetection/l515.py
+++ 03_FlightSoftware/00_Perception/01_ObjectDetection/l515.py
@@ -1,26 +1,61 @@
 import pyrealsense2 as rs
+import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 class L515:
-    def __init__(self, frame_width, frame_height) -> None:
-        # output dims
-        self.frame_width = frame_width
-        self.frame_height = frame_height
+    def __init__(self, output_width, output_height) -> None:
+        # config dims (LIDAR-IR)
+        self.L515_LIDAR_IR_CONFIG_WIDTH = 320
+        self.L515_LIDAR_IR_CONFIG_HEIGHT = 240
 
-        # L515-config 
+        # config dims (RGB)
+        self.L515_COLOR_CONFIG_WIDTH = 640
+        self.L515_COLOR_CONFIG_HEIGHT = 480
+
+        # output dims
+        self.output_width = output_width
+        self.output_height = output_height
+
+        # L515-config settings
         self.pipeline = rs.pipeline()
         self.config = rs.config()
-        self.config.enable_stream(rs.stream.color, self.frame_width, self.frame_height, rs.format.rgb8, 30)
 
-        # start camera with applied configurations
+        # enable RGB / LIDAR-Depth / Infrared-NV
+        self.config.enable_stream(rs.stream.color, self.L515_COLOR_CONFIG_WIDTH, self.L515_COLOR_CONFIG_HEIGHT, rs.format.rgb8, 30)
+        self.config.enable_stream(rs.stream.depth, self.L515_LIDAR_IR_CONFIG_WIDTH, self.L515_LIDAR_IR_CONFIG_HEIGHT, rs.format.z16, 30)
+        self.config.enable_stream(rs.stream.infrared, self.L515_LIDAR_IR_CONFIG_WIDTH, self.L515_LIDAR_IR_CONFIG_HEIGHT, rs.format.y8, 30)
+
+        # apply settings
         self.pipeline.start(self.config)
 
     def GetRGB8(self):
         frames = self.pipeline.wait_for_frames()
-
-        # select color frame from L515-output frames
         color_frame = frames.get_color_frame()
+
         color_image = np.asanyarray(color_frame.get_data())
+        color_image = cv2.resize(color_image, (self.output_width, self.output_height), interpolation=cv2.INTER_NEAREST)
 
         return color_image
+
+    def GetDepth16(self):
+        frames = self.pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+
+        depth_map = np.asanyarray(depth_frame.get_data())
+        depth_map = cv2.applyColorMap(cv2.convertScaleAbs(depth_map, alpha=0.03), cv2.COLORMAP_JET)
+        depth_map = cv2.resize(depth_map, (self.output_width, self.output_height), interpolation=cv2.INTER_NEAREST)
+
+        return depth_map
+
+    def GetInfrared8(self):
+        frames = self.pipeline.wait_for_frames()
+        ir_frame = frames.get_infrared_frame()
+
+        ir_image = np.asanyarray(ir_frame.get_data())
+        ir_image = cv2.resize(ir_image, (self.output_width, self.output_height), interpolation=cv2.INTER_NEAREST)
+        
+        return ir_image
+    
+
     
